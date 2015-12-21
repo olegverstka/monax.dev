4ff2819cfddaab4057241449ca4afa57e62a3128
olegverstka <olegverstka@gmail.com>
2015-12-22 00:37:07 +0300

    Сверстал полностью

--- slicing/app/js/main.js
+++ slicing/app/js/main.js
@@ -1,8 +1,19 @@
 $(document).ready(function(){
-    
-    $(".select").selecter();
+	
+	$(".select").selecter();
 
-    $(".bxSlider").bxSlider({
-        'pager' : false
-    });
+	$(".bxSlider").bxSlider({
+		'pager'        : false
+	});
+	$(".hitSlider").bxSlider({
+		'useCSS'       : false,
+		'minSlides'    : 5,
+		'maxSlides'    : 5,
+		'moveSlides'   : 1,
+		'slideWidth'   : 225,
+		'infiniteLoop' : true
+	});
+	$('.foot_menu ul').autocolumnlist({
+		columns: 2
+	});
 });
